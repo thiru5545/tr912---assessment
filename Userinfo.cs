b8aceb546287b342bb4586be697b1c08adbc531c
tr912 <tr912@THIRU>
2026-01-23 10:12:45 +0530

    Add project files.

---
+++ Userinfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+class Userinfo
+{
+    private int Id;
+    private string Username;
+    private string Password;
+    private string Email;
+    private string Subscription;
+
+    public Userinfo(int Id,string Username,string Password,string Email,string Subscription)
+    {
+        this.Id = Id;
+        this.Username = Username;
+        this.Password = Password;
+        this.Email = Email;
+        this.Subscription = Subscription;
+    }
+    public int id
+    {
+        get {  return Id; }
+        set { Id = value; }
+    }
+    public string name
+    {
+        get { return Username; }
+        set { Username = value; }
+    }
+    public string password
+    {
+        get { return Password; }
+        set { Password = value; }
+    }
+    public string email
+    {
+        get { return Email; }
+        set { Email = value; }
+    }
+    public string subscription {
+        get { return Subscription; }
+        set { Subscription = value; }
+    }
+    public override string ToString()
+    {
+        return $"USER ID : {Id} \tUSERNAME : {name} \tEMAIL : {email} \t SUBSCRIPTION : {subscription}";
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Users
+    {
+        Dictionary<int,Userinfo> userinfos = new Dictionary<int,Userinfo>();
+
+        public void adduser(int id, string name, string password, string email,string sub)
+        {
+            userinfos.Add(id,new Userinfo(id, name, password,email,sub));   
+        }
+
+        public void removeuser(int id)
+        {
+            userinfos.Remove(id);
+            Console.WriteLine("ID REMOVED SUCCESSFULLY");
+        }
+
+        public void viewall()
+        {
+            Console.WriteLine(userinfos.Count);
+            foreach (int user in userinfos.Keys)
+            {
+                Console.WriteLine(userinfos[user].ToString());
+            }
+        }
+
+        public bool login(int id, string password)
+        {
+            bool t = userinfos.ContainsKey(id);
+            if (t)
+            {
+                if (userinfos[id].password.Equals(password))
+                {
+                    Console.WriteLine("USER LOGINED");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void getuserbyid(int id)
+        {
+            //return userinfos[id];
+            Console.WriteLine(userinfos[id].ToString());
+        }
+
+
+    }
+}
